4c43c28a25389d472c9a3d3e9f124f35dc4ae769
Shpetim <shpetim.alimi@ndbit.net>
2024-12-01 20:09:11 +0100

    Refactor code to use zip and unzip and split_whitespace

--- src/day_one.rs
+++ src/day_one.rs
@@ -6,9 +6,8 @@ pub fn part_two() {
     let mut col_two: HashMap<i32, i32> = HashMap::new();
 
     file.lines().for_each(|line| {
-        line.split(" ")
+        line.split_whitespace()
             .enumerate()
-            .filter(|(_, x)| x.trim().len() != 0)
             .for_each(|(idx, num)| {
                 if let Ok(num) = num.trim().parse::<i32>() {
                     if idx % 2 == 0 {
@@ -27,36 +26,30 @@ pub fn part_two() {
     for num in col_one {
         col_two.entry(num).and_modify(|x| sum += *x * num);
     }
+
+    println!("SUM PART 2: {sum}");
 }
 
 pub fn part_one() {
     let file = read_to_string("locations.txt").expect("File doesn't exist");
-    let mut col_one: Vec<i32> = vec![];
-    let mut col_two: Vec<i32> = vec![];
-
-    file.lines().for_each(|line| {
-        line.split(" ")
-            .enumerate()
-            .filter(|(_, x)| x.trim().len() != 0)
-            .for_each(|(idx, num)| {
-                if let Ok(num) = num.trim().parse::<i32>() {
-                    if idx % 2 == 0 {
-                        col_two.push(num);
-                    } else {
-                        col_one.push(num);
-                    }
-                }
-            })
-    });
+    let (mut col_one, mut col_two): (Vec<i32>, Vec<i32>) = file
+        .lines()
+        .map(|line| {
+            let parts: Vec<&str> = line.split_whitespace().collect();
+            (
+                parts[0].parse::<i32>().unwrap(),
+                parts[1].parse::<i32>().unwrap(),
+            )
+        })
+        .unzip();
 
     col_one.sort();
     col_two.sort();
 
     let mut sum = 0;
 
-    for (idx, number) in col_one.iter().enumerate() {
-        let distance = (number - col_two[idx]).abs();
-        sum += distance;
+    for (x, y) in col_one.iter().zip(col_two.iter()) {
+        sum += (x - y).abs();
     }
 
     println!("SUM {}", sum);
