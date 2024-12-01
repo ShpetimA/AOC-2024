ebef8dafe272ae57a5611c281e36e16f8b78f38f
Shpetim <shpetim.alimi@ndbit.net>
2024-12-01 20:17:27 +0100

    Time elapsed

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use std::time::Instant;
+
 use day_one::{part_one, part_two};
 
 mod day_one;
 
 fn main() {
+    let now = Instant::now();
     part_one();
+    let elapsed = now.elapsed().as_micros();
+    println!("Time {elapsed}");
+
+    let now = Instant::now();
     part_two();
+    let elapsed = now.elapsed().as_micros();
+    println!("Time 2 {elapsed}");
 }
